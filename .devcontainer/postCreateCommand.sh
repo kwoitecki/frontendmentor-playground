529cdbd21acdfb493fafc17c2f3d01e8a7082f7b
Kim Woitecki <k.woitecki@googlemail.com>
2024-04-03 11:46:18 +0000

    initial setup social links profile challenge

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,3 @@
 sudo chown node /workspace/challenges/newbie/qr-code-component/node_modules
 sudo chown node /workspace/challenges/newbie/blog-preview-card/node_modules
+sudo chown node /workspace/challenges/newbie/social-links-profile/node_modules
