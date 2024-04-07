fd34426fbd81eadab44dd08ba409076e004f3373
Kim Woitecki <k.woitecki@googlemail.com>
2024-04-07 11:11:15 +0000

    3 column preview card component - init

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,4 @@ sudo chown node /workspace/challenges/newbie/qr-code-component/node_modules
 sudo chown node /workspace/challenges/newbie/blog-preview-card/node_modules
 sudo chown node /workspace/challenges/newbie/social-links-profile/node_modules
 sudo chown node /workspace/challenges/newbie/recipe-page/node_modules
+sudo chown node /workspace/challenges/newbie/3-column-preview-card-component/node_modules
