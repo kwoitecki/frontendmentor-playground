a17f51a9fd17b8a93be606c709e2264d0c629574
Kim Woitecki <k.woitecki@googlemail.com>
2024-04-15 16:19:17 +0000

    initial setup faq accordion

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,3 +3,4 @@ sudo chown node /workspace/challenges/newbie/blog-preview-card/node_modules
 sudo chown node /workspace/challenges/newbie/social-links-profile/node_modules
 sudo chown node /workspace/challenges/newbie/recipe-page/node_modules
 sudo chown node /workspace/challenges/newbie/3-column-preview-card-component/node_modules
+sudo chown node /workspace/challenges/newbie/faq-accordion/node_modules
