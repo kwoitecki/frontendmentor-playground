221d1c34270be8a81e1b5a4efe84f47e2e2278c0
Kim Woitecki <k.woitecki@googlemail.com>
2024-04-22 18:36:16 +0000

    initial setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,4 @@ sudo chown node /workspace/challenges/newbie/social-links-profile/node_modules
 sudo chown node /workspace/challenges/newbie/recipe-page/node_modules
 sudo chown node /workspace/challenges/newbie/3-column-preview-card-component/node_modules
 sudo chown node /workspace/challenges/newbie/faq-accordion/node_modules
+sudo chown node /workspace/challenges/newbie/interactive-rating-component/node_modules
---
+++ challenges/newbie/interactive-rating-component/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hallo Welt</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
