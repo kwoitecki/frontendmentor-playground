a497c77d50fdf484b66008b30c801ee42840fc84
Kim Woitecki <k.woitecki@googlemail.com>
2024-04-28 07:38:32 +0000

    initial setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,3 +5,4 @@ sudo chown node /workspace/challenges/newbie/recipe-page/node_modules
 sudo chown node /workspace/challenges/newbie/3-column-preview-card-component/node_modules
 sudo chown node /workspace/challenges/newbie/faq-accordion/node_modules
 sudo chown node /workspace/challenges/newbie/interactive-rating-component/node_modules
+sudo chown node /workspace/challenges/newbie/apparel-coming-soon/node_modules
---
+++ challenges/newbie/apparel-coming-soon/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <h1>Frontend Mentor | Base Apparel coming soon page</h1>
+    </>
+  );
+}
+
+export default App;
