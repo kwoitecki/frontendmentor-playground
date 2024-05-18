19f77e57bbdf8dbe1794ee77361b430892fccbef
Kim Woitecki <k.woitecki@googlemail.com>
2024-05-18 09:34:33 +0000

    added typography

--- portfolio/skilled-elearning-landing-page/src/App.tsx
+++ portfolio/skilled-elearning-landing-page/src/App.tsx
@@ -3,7 +3,15 @@ import './App.scss';
 function App() {
   return (
     <>
-      <h1>Frontend Mentor | Skilled e-learning landing page</h1>
+      <header>
+        <h1>Test1</h1>
+        <h2>Test2</h2>
+        <h3>Test3</h3>
+        <h4>Test4</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, esse.
+        </p>
+      </header>
     </>
   );
 }
