f16b3f818b4de72ce11cee01d88cce97bc2aaa88
Kim Woitecki <k.woitecki@googlemail.com>
2024-06-01 10:37:04 +0000

    final

--- premium/skilled-elearning-landing-page/src/components/PageContent.tsx
+++ premium/skilled-elearning-landing-page/src/components/PageContent.tsx
@@ -2,11 +2,6 @@ import Courses from './Courses';
 import PageHero from './PageHero';
 
 const PageContent = () => {
-  const testStyle = {
-    background: 'black',
-    color: 'white',
-  };
-
   return (
     <main>
       <PageHero />
