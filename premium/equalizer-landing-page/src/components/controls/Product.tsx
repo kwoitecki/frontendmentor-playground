66e18152ba1df1897fbd0728188d1bc9e4dadf59
Kim Woitecki <k.woitecki@googlemail.com>
2024-06-08 12:55:26 +0000

    202406 - 1500 working draft

--- premium/equalizer-landing-page/src/components/controls/Product.tsx
+++ premium/equalizer-landing-page/src/components/controls/Product.tsx
@@ -1,20 +1,37 @@
 const Product = () => {
   return (
     <article className='product'>
-      <img src='./illustration-app.png' alt='Illustration of the app' />
-      <div>
-        <h2>Premium EQ</h2>
-        <p>
+      <img
+        className='product__image'
+        src='./illustration-app.png'
+        alt='Illustration of the app'
+      />
+      <picture>
+        <img
+          className='product__background'
+          src='./bg-pattern-2.svg'
+          alt='Product Background'
+        />
+      </picture>
+      <div className='product__content'>
+        <h2 className='product__header'>Premium EQ</h2>
+        <p className='product__description'>
           Get expert-level control with a robust equalizer, volume mixer, and
           spatial audio. Take your listening experience to a whole new level and
           access all our incredible features!
         </p>
-        <p>
-          <span>$4</span> / month
+        <p className='product__price'>
+          <span className='product__price-acent'>$4</span> / month
         </p>
-        <div>
-          <button>iOS Download</button>
-          <button>Android Download</button>
+        <div className='product__actions'>
+          <button className='btn btn--dark'>
+            <img src='./icon-apple.svg' alt='apple download' />
+            iOS Download
+          </button>
+          <button className='btn'>
+            <img src='./icon-android.svg' alt='android download' />
+            Android Download
+          </button>
         </div>
       </div>
     </article>
