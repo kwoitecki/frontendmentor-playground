e22086535b28ea01c5b9d17a6751716c6574ca8f
Kim Woitecki <k.woitecki@googlemail.com>
2024-06-09 07:15:14 +0000

    mobile ground structure

--- premium/equalizer-landing-page/src/components/PageFooter.tsx
+++ premium/equalizer-landing-page/src/components/PageFooter.tsx
@@ -1,17 +1,18 @@
 const PageFooter = () => {
   return (
-    <footer>
-      <div>
-        <img src='./logo.svg' alt='logo equalizer' />
+    <footer className='footer'>
+      <div className='footer__image-text'>
+        <img className='footer__image' src='./logo.svg' alt='logo equalizer' />
+
+        <div className='footer_text'>
+          <p>All rights reserved © Equalizer 2021</p>
+          <p>
+            Have any problems? Contact us via social media or email us at
+            <span className='footer_text-accent'> equalizer@example.com</span>
+          </p>
+        </div>
       </div>
-      <div>
-        <p>All rights reserved © Equalizer 2021</p>
-        <p>
-          Have any problems? Contact us via social media or email us at
-          <span>equalizer@example.com</span>
-        </p>
-      </div>
-      <div>
+      <div className='footer__social'>
         <img src='./icon-facebook.svg' alt='Facebook icon' />
         <img src='./icon-instagram.svg' alt='Instagram icon' />
         <img src='./icon-twitter.svg' alt='Twitter icon' />
