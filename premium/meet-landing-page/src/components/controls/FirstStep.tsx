665d1b8338a3eb3963e312170ecc0659cddd1a13
Kim Woitecki <k.woitecki@googlemail.com>
2024-06-22 04:33:41 +0000

    mobile design - final

--- premium/meet-landing-page/src/components/controls/FirstStep.tsx
+++ premium/meet-landing-page/src/components/controls/FirstStep.tsx
@@ -3,20 +3,42 @@ const StepOne = () => {
     <section className='step'>
       <img className='img-setup' src='step1.png' alt='first step' />
 
-      <div className='step1__images'>
-        <img className='img-setup' src='image-woman-in-videocall.jpg' />
-        <img className='img-setup' src='image-women-videochatting.jpg' />
-        <img className='img-setup' src='image-men-in-meeting.jpg' />
-        <img className='img-setup' src='image-man-texting.jpg' />
-      </div>
+      <article className='step1__images'>
+        <img
+          className='img-setup'
+          src='image-woman-in-videocall.jpg'
+          alt='use videocalls'
+        />
+        <img
+          className='img-setup'
+          src='image-women-videochatting.jpg'
+          alt='use videchatting'
+        />
+        <img
+          className='img-setup'
+          src='image-men-in-meeting.jpg'
+          alt='use online meetings'
+        />
+        <img
+          className='img-setup'
+          src='image-man-texting.jpg'
+          alt='use texting'
+        />
+      </article>
 
-      <span>Built for modern use</span>
-      <h3>Smarter meetings, all in one place</h3>
-      <p>
-        Send messages, share files, show your screen, and record your meetings —
-        all in one workspace. Control who can join with invite-only team access,
-        data encryption, and data export.
-      </p>
+      <article className='step1-description'>
+        <span className='step1-description__teaser u-fc-secondary1-400'>
+          Built for modern use
+        </span>
+        <h2 className='step1-description__heading'>
+          Smarter meetings, all in one place
+        </h2>
+        <p className='step1-description__text u-fc-primary-600'>
+          Send messages, share files, show your screen, and record your meetings
+          — all in one workspace. Control who can join with invite-only team
+          access, data encryption, and data export.
+        </p>
+      </article>
     </section>
   );
 };
--- premium/meet-landing-page/src/components/controls/SecondStep.tsx
+++ premium/meet-landing-page/src/components/controls/SecondStep.tsx
@@ -1,15 +1,19 @@
 const SecondStep = () => {
   return (
-    <section className='step'>
+    <section className='step step2'>
       <img className='img-setup' src='step2.png' alt='second step' />
-      <h3>Experience more together</h3>
-      <p>
-        Stay connected with reliable HD meetings and unlimited one-on-one and
-        group video sessions.
-      </p>
-      <button className='btn btn--large u-bg-secondary2-500 u-fc-primary-900'>
-        Download <span className='u-fc-secondary2-800'>v1.3</span>
-      </button>
+      <article className='step2-content u-bg-secondary1-400 u-fc-primary-900'>
+        <h2 className='step2-content__title'>Experience more together</h2>
+        <p className='step2-content__text'>
+          Stay connected with reliable HD meetings and unlimited one-on-one and
+          group video sessions.
+        </p>
+        <div className='step2-content__actions'>
+          <button className='btn btn--large u-bg-secondary2-500 u-fc-primary-900'>
+            Download <span className='u-fc-secondary2-800'>v1.3</span>
+          </button>
+        </div>
+      </article>
     </section>
   );
 };
