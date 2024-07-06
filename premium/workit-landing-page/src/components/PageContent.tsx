53aea14d6bb367100aa149fdc40234949bb8a6bb
Kim Woitecki <k.woitecki@googlemail.com>
2024-07-06 08:50:38 +0000

    mobile design

--- premium/workit-landing-page/src/components/PageContent.tsx
+++ premium/workit-landing-page/src/components/PageContent.tsx
@@ -3,7 +3,7 @@ import Card from './controls/Card';
 
 const PageContent = () => {
   return (
-    <main className='bg-pattern-light main-content'>
+    <main className='main-content'>
       <article className='cards'>
         {cards.map((card) => (
           <Card key={card.id} {...card} />
@@ -15,14 +15,18 @@ const PageContent = () => {
           className='apply__image img-clear'
           alt='Apply for access'
         />
-        <h2 className='heading heading--medium'>Be the first to test</h2>
-        <p className='content'>
-          Hi, I'm Louis Graham, the founder of the company. Book a demo call
-          with me to become a beta tester for our app and kickstart your
-          company. Apply for access below and I’ll be in touch to schedule a
-          call.
-        </p>
-        <button className='button button--primary'>Apply for access</button>
+        <div className='u-bg-primary-100 u-fc-primary-1000 apply__content'>
+          <h2 className='heading heading--medium'>Be the first to test</h2>
+          <p className='content apply__content-description'>
+            Hi, I'm Louis Graham, the founder of the company. Book a demo call
+            with me to become a beta tester for our app and kickstart your
+            company. Apply for access below and I’ll be in touch to schedule a
+            call.
+          </p>
+          <button className='button button--primary apply__content-action'>
+            Apply for access
+          </button>
+        </div>
       </article>
     </main>
   );
