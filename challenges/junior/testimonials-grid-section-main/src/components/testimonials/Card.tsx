71a740c4947c3198f460d6c1320c1d0c7f6bf8d0
Kim Woitecki <k.woitecki@googlemail.com>
2024-07-15 06:52:54 +0000

    challange - final

--- challenges/junior/testimonials-grid-section-main/src/components/testimonials/Card.tsx
+++ challenges/junior/testimonials-grid-section-main/src/components/testimonials/Card.tsx
@@ -3,11 +3,11 @@ import styles from './card.module.scss';
 
 const Card: FC<TestimonialProps> = ({ name, role, image, title, content }) => (
   <div className={styles.card}>
-    <img src={image} alt={name} />
-    <h2>{name}</h2>
-    <h3>{role}</h3>
-    <h4>{title}</h4>
-    <p>{content}</p>
+    <img src={image} alt={name} className={styles.card__image} />
+    <p className={styles.card__name}>{name}</p>
+    <p className={styles.card__role}>{role}</p>
+    <h2 className={styles.card__title}>{title}</h2>
+    <q className={styles.card__content}>{content}</q>
   </div>
 );
 
