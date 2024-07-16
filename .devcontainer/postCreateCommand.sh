8b7b16aa3f18aab358be65837ecf3d3ec7857c78
Kim Woitecki <k.woitecki@googlemail.com>
2024-07-16 10:45:37 +0000

    initial setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -15,3 +15,4 @@ sudo chown node /workspace/premium/equalizer-landing-page/node_modules
 sudo chown node /workspace/premium/meet-landing-page/node_modules
 sudo chown node /workspace/premium/workit-landing-page/node_modules
 sudo chown node /workspace/challenges/junior/testimonials-grid-section-main/node_modules
+sudo chown node /workspace/challenges/junior/time-tracking-dashboard-main/node_modules
---
+++ challenges/junior/time-tracking-dashboard-main/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <h1>Frontend Mentor | Time tracking dashboard</h1>
+    </>
+  );
+}
+
+export default App;
