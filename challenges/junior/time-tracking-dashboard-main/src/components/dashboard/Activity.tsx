bca66a3349af6141f489ad1ac1481ac1a9a02f0c
Kim Woitecki <k.woitecki@googlemail.com>
2024-07-17 03:40:09 +0000

    implemented base functionality

--- challenges/junior/time-tracking-dashboard-main/src/components/dashboard/Activity.tsx
+++ challenges/junior/time-tracking-dashboard-main/src/components/dashboard/Activity.tsx
@@ -5,19 +5,39 @@ type ActivityCardProps = ActivityProps & {
   currentTimeframe: Timeframe;
 };
 
+type TimeframeProps = {
+  current: number;
+  previous: number;
+};
+
 const Activity: FC<ActivityCardProps> = ({
   title,
   timeframes,
   currentTimeframe,
 }) => {
-  // todo: set data depending on the current timeframe
+  let value: Partial<TimeframeProps> = {
+    current: 0,
+    previous: 0,
+  };
+
+  if (timeframes && currentTimeframe) {
+    switch (currentTimeframe) {
+      case Timeframe.daily:
+        value = timeframes.daily;
+        break;
+      case Timeframe.monthly:
+        value = timeframes.monthly;
+        break;
+      default:
+        value = timeframes.weekly;
+    }
+  }
 
   return (
     <div>
       <h3>{title}</h3>
-      <p>{timeframes?.weekly.current}</p>
-      <p>{timeframes?.weekly.previous}</p>
-      <p>{currentTimeframe}</p>
+      <p>{value.current}</p>
+      <p>{value.previous}</p>
     </div>
   );
 };
